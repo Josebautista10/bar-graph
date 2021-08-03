82927ff16776d4250c1d80163b749c6aaa788ee3
Jose Bautista <Cooldj1020@gmail.com>
2021-08-03 21:08:42 +0100

    displaying bars

--- src/drawBarChart.js
+++ src/drawBarChart.js
@@ -6,20 +6,30 @@ export function drawBarChart(data, options, element) {
   const { barSpacing, switchAxes } = options[options.length - 1];
   const spacingScale = 5;
   const maxHeight = Math.max(...data);
+  const minHeight = Math.min(...data);
+  console.log(minHeight)
+  const maxGraphHeightPx = 500
+  const scale = maxGraphHeightPx / minHeight;
+
 
   canvas.className = 'axis';
 
   for (let i = 0; i < totalBars; i++) {
-    const bar = document.createElement('li');
+    const bar = document.createElement('div');
 
     bar.className = 'bar';
     bar.id = `bar-${i + 1}`;
-    bar['data-value'] = totalBars[i];
     bar.style['background-color'] = options[i].color;
-    bar.style.width = '20%';
-    bar.style.height = '100%';
+    bar.style.width = '20px';
+    bar.style["margin-left"] = totalBars === 1 ? '300px' : `${barSpacing}px`
+    bar.style["margin-right"] = `${barSpacing}px`
 
-    canvas.querySelector('ul').appendChild(bar);
+    const barHeight = data[i] * maxGraphHeightPx / maxHeight
+    bar.style.height = `${barHeight}px`
+    bar.style["margin-top"] = `${maxGraphHeightPx - barHeight}px`
+
+
+    canvas.querySelector('.bars').appendChild(bar);
   }
 
   console.log(canvas);

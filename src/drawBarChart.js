8a594f72b05c0b8e2b7dd89d54e296be03eca7c4
Jose Bautista <Cooldj1020@gmail.com>
2021-08-03 22:15:18 +0100

    adding defualt color and spacing

--- src/drawBarChart.js
+++ src/drawBarChart.js
@@ -3,14 +3,23 @@ export function drawBarChart(data, options, element) {
   drawButton.disabled = true;
   const canvas = document.querySelector(`#${element}`);
   const totalBars = data.length;
-  const { barSpacing, switchAxes } = options[options.length - 1];
-  const spacingScale = 5;
   const maxHeight = Math.max(...data);
-  const minHeight = Math.min(...data);
-  console.log(minHeight)
-  const maxGraphHeightPx = 500
-  const scale = maxGraphHeightPx / minHeight;
+  const maxGraphHeightPx = 500;
+  const maxGraphWidthPx = 1000;
+  const yAxisScale = maxGraphHeightPx / maxHeight;
+  const xAxisScale = maxGraphWidthPx / totalBars - (totalBars + 1);
+  const defaultBarSettings = {
+    colors: ['red', 'green', 'blue', 'yellow'],
+    barSpacing: 5
+  };
 
+  let { barSpacing = 5, switchAxes } = options[options.length - 1];
+  
+  if (isNaN(barSpacing)) {
+    barSpacing = defaultBarSettings.barSpacing
+  }
+
+   
 
   canvas.className = 'axis';
 
@@ -19,15 +28,16 @@ export function drawBarChart(data, options, element) {
 
     bar.className = 'bar';
     bar.id = `bar-${i + 1}`;
-    bar.style['background-color'] = options[i].color;
-    bar.style.width = '20px';
-    bar.style["margin-left"] = totalBars === 1 ? '300px' : `${barSpacing}px`
-    bar.style["margin-right"] = `${barSpacing}px`
+    const randomColor = parseInt(defaultBarSettings.colors.length * Math.random());
+    bar.style['background-color'] = options[i].color || defaultBarSettings.colors[randomColor];
+    bar.style.width = totalBars === 1 ? '250px' : `${xAxisScale}px`;
 
-    const barHeight = data[i] * maxGraphHeightPx / maxHeight
-    bar.style.height = `${barHeight}px`
-    bar.style["margin-top"] = `${maxGraphHeightPx - barHeight}px`
+    bar.style['margin-left'] = totalBars === 1 ? '300px' : `${barSpacing}px`;
+    bar.style['margin-right'] = `${barSpacing}px`;
 
+    const barHeight = data[i] * yAxisScale;
+    bar.style.height = `${barHeight}px`;
+    bar.style['margin-top'] = `${maxGraphHeightPx - barHeight}px`;
 
     canvas.querySelector('.bars').appendChild(bar);
   }
